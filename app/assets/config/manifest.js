262140e3a64d32b94bfb9e6d62b6525312ca18a6
chloe lanquetin <chloelanquetin@gmail.com>
2026-02-26 12:11:35 +0100

    install

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,9 @@
+//= link_tree ../images
+//= link_tree ../../javascript .js
+//= link_directory ../stylesheets .css
+
+//= link_tree ../images
+//= link_tree ../../javascript .js
+//= link_directory ../stylesheets .css
+//= link popper.js
+//= link bootstrap.min.js
